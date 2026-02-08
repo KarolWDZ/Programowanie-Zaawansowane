d19fe877d4bc3dbeae87ae6df320afe275fd31d6
KarolWDZ <122998576+KarolWDZ@users.noreply.github.com>
2026-02-08 17:10:12 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="my_awesome_lib",
+    version="1.0.0",
+    description="Prosta biblioteka z funkcjami do danych, matematyki i tekstu.",
+    author="Karol",
+    packages=find_packages(),
+    python_requires=">=3.8",
+)
